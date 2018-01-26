e37aed2e50ed2239083780cc2b1fc2cfeffc6f84
tokui <tokui@1-10.com>
2018-01-26 19:15:55 +0900

    first commit

---
+++ Assets/02_ArrayCS/ArrayCS.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayCS : MonoBehaviour {
+
+    [SerializeField]
+    ComputeShader _ComputeShader;
+
+    ComputeBuffer _IntBuffer;
+
+    const int totalCount = 10;
+    const int ThreadSize = 8;
+    const int GroupSize = totalCount / ThreadSize + 1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+        Debug.Log(GroupSize);
+        // init buffer
+        _IntBuffer = new ComputeBuffer(totalCount, sizeof(int));
+
+        // ComputeShader
+        int kernelId = _ComputeShader.FindKernel("Test");
+        //uint threadSizeX, threadSizeY, threadSizeZ;
+        //_ComputeShader.GetKernelThreadGroupSizes(kernelId, out threadSizeX, out threadSizeY, out threadSizeZ);
+        _ComputeShader.SetBuffer(kernelId, "_IntBuffer", _IntBuffer);
+        _ComputeShader.Dispatch(kernelId, GroupSize, 1, 1);
+
+        // 計算結果を表示
+        int[] result = new int[totalCount];
+        _IntBuffer.GetData(result);
+
+        for (int i = 0; i < totalCount; i++)
+        {
+            Debug.Log(result[i]);
+        }
+
+    }
+}
